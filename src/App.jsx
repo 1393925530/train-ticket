178a59895584405c563453a756a147e3f03d3e3a
1393925530 <zzc1393925530@gmail.com>
2021-07-07 17:27:39 +0800

    ✨ feat(lazy/Suspense): lazy/Suspense使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import { createContext, useState } from 'react'
+import { lazy, Suspense, useState } from 'react'
 import './App.css'
 
-const BatteryContext = createContext()
-const OnlineContext = createContext()
+const About = lazy(() => import(/* webpackChunkName: "about" */ './About.jsx'))
 
-function Leaf() {
-  return (
-    <BatteryContext.Consumer>
-      {(battery) => (
-        <OnlineContext.Consumer>
-          {(online) => (
-            <h1>
-              Battery: {battery}, Online: {String(online)}
-            </h1>
-          )}
-        </OnlineContext.Consumer>
-      )}
-    </BatteryContext.Consumer>
-  )
-}
-
-function Middle() {
-  return <Leaf />
-}
+// ErrorBoundary
+// componentDidCatch
 
 function App() {
-  const [battery, setBattery] = useState(60)
-  const [online, setOnline] = useState(false)
-  return (
-    <BatteryContext.Provider value={battery}>
-      <OnlineContext.Provider value={online}>
-        <button type="button" onClick={() => setBattery(battery - 1)}>
-          Press
-        </button>
-        <button type="button" onClick={() => setOnline(!online)}>
-          Switch
-        </button>
-        <Middle />
-      </OnlineContext.Provider>
-    </BatteryContext.Provider>
+  const [error, setError] = useState(false)
+  return !error ? (
+    <div>
+      <Suspense fallback={<div>loading</div>}>
+        <About></About>
+      </Suspense>
+    </div>
+  ) : (
+    <div>error</div>
   )
 }
 
