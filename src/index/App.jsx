2f9ea75862cd4a422ec252e42467de260eff4342
1393925530 <zzc1393925530@gmail.com>
2021-07-15 16:33:02 +0800

    ✨ feat(Header组件/城市选择): 城市选择组件实现

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import './App.css'
 
@@ -8,23 +9,45 @@ import HighSpeed from './HighSpeed'
 import Journey from './Journey'
 import Submit from './Submit'
 
+import { exchangeFromTo, showCitySelector } from './actions'
+
 function App(props) {
+  const { from, to, dispatch } = props
+
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
+
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector,
+      },
+      dispatch
+    )
+  }, [])
+
   return (
     <div>
-      <Header />
-      <Journey />
-      <DepartDate />
-      <HighSpeed />
-      <Submit />
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack} />
+      </div>
+      <form className="form">
+        <Journey from={from} to={to} {...cbs} />
+        <DepartDate />
+        <HighSpeed />
+        <Submit />
+      </form>
     </div>
   )
 }
 
 export default connect(
   function mapStateToProps(state) {
-    return {}
+    return state
   },
   function mapDispatchToProps(dispatch) {
-    return {}
+    return { dispatch }
   }
 )(App)
